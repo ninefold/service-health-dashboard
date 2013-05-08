8d00ce09813868ec2ee796cf3da7d527afe417a4
Jason Gellatly <jason@ninefold.com>
2013-05-09 09:50:15 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServiceHealthDashboard::Application.config.secret_token = 'fd56ba8a66cd60d945950d7534216ded8d477a831f1c38d4b46d383adb2dffe9856fdb86eb15a78bdc42728ffb738f8bab55945b821142703f8e64fa5af8fa31'
