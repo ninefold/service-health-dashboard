97f5c681f658c79e17e05f1b7a07bc08eeeb88e6
Jason Gellatly <jason@ninefold.com>
2013-05-16 14:27:00 +1000

    ready for aesthetics

---
+++ spec/features/nf1_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "NF1 Page" do
+  describe "When accessing the /1 URL" do
+  	it "should display some events" do
+  		e = Event.create(
+        "service_id"=>1,
+        "status_id"=>1,
+        "message"=>"This is a test",
+        "start"=>DateTime.now,
+        "informational"=>false,
+        "event_type_id"=>1,
+        "invisible"=>false,
+        "description"=>"TEST"
+      )
+  		visit events_path
+  		expect(page).to have_content 'This is a test'
+    end
+  end
+
+  describe "When creating a new event" do
+    it "should display the event" do
+      visit new_event_path
+      fill_in 'event_message', :with=>'This is a test message'
+      click_button "Create Event"
+      expect(page).to have_content 'This is a test message'
+    end
+  end
+
+end
