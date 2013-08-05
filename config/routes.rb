790d6cdb5d5fcfad09e71664886ef4d5459d9296
Jason Gellatly <jason@ninefold.com>
2013-08-05 15:24:54 +1000

    devise password editing not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 ServiceHealthDashboard::Application.routes.draw do
   devise_for :users
+    as :user do
+      get 'users/edit' => 'devise/registration#edit', :as => 'edit_user_registration'
+    end
 
   get "events/index"
 
