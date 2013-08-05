9cc0ca22682f9ff9c00c669532134ed12d03f53f
Jason Gellatly <jason@ninefold.com>
2013-08-05 16:01:49 +1000

    registration workaround so random users cannot sign up, but password editing is still enabled

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 ServiceHealthDashboard::Application.routes.draw do
-  devise_for :users
-
-
+  
+  if Rails.env == 'production'
+    devise_for :users, :controllers => { :registrations => "registrations" } 
+  else
+    devise_for :users
+  end
 
   get "events/index"
 
